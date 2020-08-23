1833cf7ea55eb23b01d977e82913408a3c198e96
Tom Cooper <tcooper.uk@gmail.com>
2020-08-23 21:00:37 +0100

    Added basic brew bundle

---
+++ brew/setup.sh
@@ -0,0 +1,10 @@
+  
+#!/bin/sh
+
+if test ! $(which brew); then
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+brew update
+brew tap homebrew/bundle
+brew bundle
